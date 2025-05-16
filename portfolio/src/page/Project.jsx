4005c365ac4fcc36a8e9a5e68d094eec87132bec
shubham <learngraphic0003@gmail.com>
2025-05-17 00:42:46 +0530

    d

--- portfolio/src/page/Project.jsx
+++ portfolio/src/page/Project.jsx
@@ -243,9 +243,7 @@ const Project = () => {
         </a>
       </div>
 
-      {/* 3 */}
-
-       {/* Swiggy */}
+    
        <div className="flex flex-col sm:flex-row gap-4 mt-8 sm:gap-[80px] items-center sm:mt-20 relative">
         <div className="h-[1px] left-1/4 right-1/2 bg-[#1788ae] absolute top-1/2 hidden sm:block"></div>
         <div className="w-4 h-4 rounded-full border-[3px] border-[#459bd5] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"></div>
